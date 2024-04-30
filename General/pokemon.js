6f2a65a7dbc8194035840cfbf0ae99110f7ff029
raul <raul.soneira@cirvianum.cat>
2024-04-30 17:06:59 +0200

    commit 3

--- General/pokemon.js
+++ General/pokemon.js
@@ -8,19 +8,40 @@ function getPokemonInfo(idPokemon) {
       name: pokemon.name,
       height: pokemon.height,
       weight: pokemon.weight,
-      habilitats: pokemon.abilities,
+      habilitats: pokemon.abilities.map((ability) => ability.ability.name),
       sprites: pokemon.sprites,
     }));
 }
 
 async function mostrarInfoPokemon(id) {
   try {
-    
+    const pokemon = await getPokemonInfo(id);
+
+    const nameElement = document.getElementById("name");
+    const heightElement = document.getElementById("height");
+    const weightElement = document.getElementById("weight");
+    const abilitiesElement = document.getElementById("abilities");
+    const spriteElement = document.getElementById("sprite");
+
+    nameElement.textContent = pokemon.name;
+    heightElement.textContent = `Height: ${pokemon.height}`;
+    weightElement.textContent = `Weight: ${pokemon.weight}`;
+
+    const abilitiesList = abilitiesElement.querySelector("ul");
+    abilitiesList.innerHTML = "";
+    pokemon.habilitats.forEach((ability) => {
+      const li = document.createElement("li");
+      li.textContent = ability;
+      abilitiesList.appendChild(li);
+    });
+
+    console.log(pokemon.sprites);
+
+    spriteElement.src = ;
+    spriteElement.alt = pokemon.name;
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
 mostrarInfoPokemon(4);
-
-// getPokemonInfo(2);
