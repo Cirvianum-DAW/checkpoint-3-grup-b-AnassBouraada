00cc1dbb632401971f5a26d1986f5e85de1ad38d
raul <raul.soneira@cirvianum.cat>
2024-04-30 18:30:29 +0200

    commit 6

--- UF3/validacions.js
+++ UF3/validacions.js
@@ -2,6 +2,7 @@
 
 // Selecció dels inputs...
 const nameInput = document.getElementById("name");
+const surnameInput = document.getElementById("surname");
 
 // Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
 // els errors allà on consideris lògic
@@ -28,13 +29,83 @@ function displayError(input, message) {
 function validateName() {
   removeExistingError(nameInput);
   // Validació del nom...
-  if (nameInput.value.length < 5) {
-    console.log("2");
+  if (nameInput.value.length < 3 || nameInput.value.length > 50) {
     // Si no és vàlid...
     displayError(nameInput, "El nom no és vàlid");
   }
 }
 
-// addEventListeners...
+function validateSurname() {
+  removeExistingError(surnameInput);
+  // Validació del nom...
+  if (surnameInput.value.length < 3 || surnameInput.value.length > 50) {
+    // Si no és vàlid...
+    displayError(surnameInput, "El cognom no és vàlid");
+  }
+}
 
+// addEventListeners...
+surnameInput.addEventListener("blur", validateSurname);
 nameInput.addEventListener("blur", validateName);
+
+// Selecció dels inputs...
+const genderInput = document.getElementById("gender");
+const ageInput = document.getElementById("age");
+
+// Funció per validar el gènere
+function validateGender() {
+  removeExistingError(genderInput);
+  // Validació del gènere...
+  if (genderInput.value === "") {
+    // Si no és vàlid...
+    displayError(genderInput, "Si us plau, selecciona el teu gènere");
+  }
+}
+
+// Funció per validar l'edat
+function validateAge() {
+  // Convertim l'edat a un nombre enter
+  const age = parseInt(ageInput.value);
+  // Validació de l'edat...
+  if (isNaN(age) || age < 18) {
+    // Si no és vàlid...
+    alert("Has de ser major d'edat per poder realitzar la comanda");
+  }
+}
+
+// Afegim event listeners per a la validació del gènere i l'edat
+genderInput.addEventListener("change", validateGender);
+ageInput.addEventListener("blur", validateAge);
+
+const messageInput = document.getElementById("message");
+
+// Funció per validar el missatge
+function validateMessage() {
+  removeExistingError(messageInput);
+  // Validació del missatge...
+  if (messageInput.value.length < 10 || messageInput.value.length > 200) {
+    // Si no és vàlid...
+    displayError(
+      messageInput,
+      "El text a ha de tenir com a mínim 10 caràcters i com a màxim 200"
+    );
+  }
+}
+
+messageInput.addEventListener("input", validateMessage);
+
+const emailInput = document.getElementById("email");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Funció per validar l'email
+function validateEmail() {
+  removeExistingError(emailInput);
+  // Validació de l'edat...
+  if (!emailRegex.test(emailInput)) {
+    // Si no és vàlid...
+    alert("l'email ha de tenir el format correcte");
+  }
+}
+
+emailInput.addEventListener("blur", validateEmail);
