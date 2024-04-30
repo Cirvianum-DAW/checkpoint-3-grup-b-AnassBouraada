021afa22aa39e98694b5049bf7b72e2f349dd118
raul <raul.soneira@cirvianum.cat>
2024-04-30 16:17:54 +0200

    commit 2

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,9 +1,26 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
 
-function getPokemon(idPokemon) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`).then((res) =>
-    res.json()
-  );
+function getPokemonInfo(idPokemon) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+    .then((res) => res.json())
+    .then((pokemon) => ({
+      id: pokemon.id,
+      name: pokemon.name,
+      height: pokemon.height,
+      weight: pokemon.weight,
+      habilitats: pokemon.abilities,
+      sprites: pokemon.sprites,
+    }));
 }
 
+async function mostrarInfoPokemon(id) {
+  try {
+    
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
+mostrarInfoPokemon(4);
+
+// getPokemonInfo(2);
